8c2a89e2369427f33f3a43fd730df262578bbfe8
Joe Bloggs <vapniks@yahoo.com>
2021-10-05 00:46:42 +0100

    rename: fzf-tools -> fzf-tool

--- fzf_tool_launcher.sh
+++ fzf_tool_launcher.sh
@@ -13,18 +13,18 @@ function fzf-tool-launcher() {
     fi
     typeset preview='f={} && if' tools='f={} && if' k v
     typeset -a filetypes
-    zstyle -g filetypes ':fzf-tools-launcher:previewcmd:'
+    zstyle -g filetypes ':fzf-tool-launcher:previewcmd:'
     local maxsize t tmp
-    zstyle -s ':fzf-tools-launcher:' max_preview_size maxsize || maxsize=10000000
+    zstyle -s ':fzf-tool-launcher:' max_preview_size maxsize || maxsize=10000000
     foreach t (${filetypes}) {
-	zstyle -s ':fzf-tools-launcher:previewcmd:' "${t}" tmp
+	zstyle -s ':fzf-tool-launcher:previewcmd:' "${t}" tmp
 	preview+=" [ -z \"\${f%%*${t}}\" ];then ${tmp};elif"
     }
     local condstr="[ \$(stat -c '%s' {}) -gt ${maxsize} ]"
     preview="if ${condstr};then head -c${maxsize} {};echo \"\n\nTRUNCATED TO FIRST ${maxsize} BYTES\";else {${preview%%elif}else cat {};fi||cat {}};fi"
     local header="ctrl-g=quit:ctrl-v=view raw:alt-v=view formatted:enter=choose tool:ctrl-j=print filename"
     local toolsmenu 
-    zstyle -s ':fzf-tools-launcher:' toolsmenu toolsmenu || toolsmenu="~/.fzfrepl/tools_menu"
+    zstyle -s ':fzf-tool-launcher:' toolsmenu toolsmenu || toolsmenu="~/.fzfrepl/tools_menu"
     tools="sed '/#/d;/^\s*\$/d' ${toolsmenu}|fzf --with-nth=1 --preview-window=down:3:wrap --preview='echo \{2..}|sed s@\{\}@{}@' --bind='enter:execute(tmux new-window -n \$(basename {}) -d \"\$(echo \{2..}|sed s@\{\}@{}@)\")'"
     
     local file=$(print -l ${@}|fzf --height=100% \
