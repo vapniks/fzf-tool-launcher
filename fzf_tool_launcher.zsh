56b7dedc2d2d024abf6bbb6265e64fb42b77262e
Joe Bloggs <vapniks@yahoo.com>
2021-10-06 10:53:14 +0100

    Add note about problems trying to substitute {+}

--- fzf_tool_launcher.zsh
+++ fzf_tool_launcher.zsh
@@ -25,8 +25,14 @@ function fzf-tool-launcher() {
     local header="ctrl-g=quit:ctrl-v=view raw:alt-v=view formatted:enter=choose tool:ctrl-j=print filename"
     local toolsmenu 
     zstyle -s ':fzf-tool-launcher:' tools_menu_file toolsmenu || toolsmenu="~/.fzfrepl/tools_menu"
+    # TODO: try to get {+} replacements working. Have tried all different kinds of quoting combinations, but none seem to work.
+    # The substitution works for the --preview option, but not the --bind option. To get it to work for the --preview option
+    # you have to quote the {+} replacement in the sed command, otherwise it introduces spaces which makes sed think the command
+    # is incomplete. However, when I try the same thing with the --bind command it doesn't work; fzf emits an "unknown action" error,
+    # followed by the text right after the +. fzf treats the + as an action separator (used for chaining commnds, see the docs).
+    #tools="sed '/#/d;/^\s*\$/d' ${toolsmenu}|fzf --with-nth=1 --preview-window=down:3:wrap --preview='echo \{2..}|sed -e s@\{\}@{}@g -e s@\{\+\}@\"{+}\"@g' --bind='enter:execute(tmux new-window -n test -d \"\$(echo \{2..}|sed -e s@\{\}@{}@g)\")'"
+
     tools="sed '/#/d;/^\s*\$/d' ${toolsmenu}|fzf --with-nth=1 --preview-window=down:3:wrap --preview='echo \{2..}|sed s@\{\}@{}@' --bind='enter:execute(tmux new-window -n \$(basename {}) -d \"\$(echo \{2..}|sed s@\{\}@{}@)\")'"
-    
     local file=$(print -l ${@}|fzf --height=100% \
 				   --header="${header}" \
 				   --preview="stat -c 'SIZE:%s bytes OWNER:%U GROUP:%G PERMS:%A' {} && {if ${condstr};then echo \"RAW:\";else echo \"FORMATTED:\";fi} && ${preview}" \
